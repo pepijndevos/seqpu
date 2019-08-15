5583632e3f61c8e14c5c347a1dd72958a76951be
Pepijn de Vos <pepijndevos@gmail.com>
2019-08-15 08:13:20 +0200

    remove casts

--- alu.vhd
+++ alu.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity alu is
   generic (
-    formal : boolean := false
+    formal : boolean := true
   );
   port (
     clk : in std_logic;
@@ -99,7 +99,7 @@ begin
     -- restrict reset to be a repeating sequence of 011111111011111111...
     restrict {{rst_n = '0'; (rst_n = '1')[*8]}[+]};
     -- assume that the opcode does not change while not in reset
-    assume always {rst_n = '0'; rst_n = '1'} |=>
+    assume always {rst_n = '0'; rst_n = '1'} |->
       opcode = last_op until rst_n = '0';
     -- assert that after 8 cycles each ALU op produces the correct output
     assert always {opcode = "000" and rst_n = '1'; rst_n = '0'} |->
@@ -109,15 +109,13 @@ begin
     assert always {opcode = "010" and rst_n = '1'; rst_n = '0'} |->
       y_sr = (a_sr or b_sr);
     assert always {opcode = "011" and rst_n = '1'; rst_n = '0'} |->
-      std_logic_vector(y_sr) = (std_logic_vector(a_sr) and std_logic_vector(b_sr));
+      y_sr = (a_sr and b_sr);
     assert always {opcode = "100" and rst_n = '1'; rst_n = '0'} |->
-      std_logic_vector(y_sr) = (std_logic_vector(a_sr) xor std_logic_vector(b_sr));
+      y_sr = (a_sr xor b_sr);
     assert always {opcode = "101" and rst_n = '1'; rst_n = '0'} |->
-      std_logic_vector(y_sr) = std_logic_vector(b_sr) and
-      (ci = '1') = (a_sr = b_sr);
+      y_sr = b_sr and (ci = '1') = (a_sr = b_sr);
     assert always {opcode = "110" and rst_n = '1'; rst_n = '0'} |->
-      std_logic_vector(y_sr) = std_logic_vector(a_sr) and
-      (ci = '1') = (a_sr > b_sr);
+      y_sr = a_sr and (ci = '1') = (a_sr > b_sr);
     assert always {opcode = "111" and rst_n = '1'; rst_n = '0'} |->
       y_sr = 0 and ci = '0';
   end generate;
